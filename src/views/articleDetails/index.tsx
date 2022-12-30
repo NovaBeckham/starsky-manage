d328570a57bb7e143bde86a4fa0b03662cea5aa3
heyuxin <william@rantion.com>
2022-12-30 11:24:46 +0800

    feature: addArticle

--- src/views/articleDetails/index.tsx
+++ src/views/articleDetails/index.tsx
@@ -4,31 +4,63 @@
  * @Date: 2022-09-09 15:59:22
  */
 
-import { defineComponent, ref } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import MdEditor from 'md-editor-v3'
 import 'md-editor-v3/lib/style.css'
 import $styles from './index.module.scss'
-// import { createArticle } from '@/api/articles'
+import { createArticle } from '@/api/articles'
+
+interface ArticleData {
+	id?: number
+	title?: string
+	content?: string
+	cover?: string
+	categoryName?: string
+	tagNames?: string[]
+	isTop?: number
+	type?: number
+	status?: number
+}
 
 export default defineComponent({
 	name: 'ArticleDetails',
 	setup() {
-		const text = ref('# Hello Editor')
-		const title = ref('')
+		const visible = ref(false)
+		const searchForm = reactive<ArticleData>({})
+		const categorys = reactive<string[]>([])
 
 		const openModel = () => {
-			console.log('title', title.value)
-			console.log('text.value', text.value)
+			console.log('title', searchForm.title)
+			console.log('content', searchForm.content)
+		}
+		const removeCategory = () => {
+			searchForm.categoryName = undefined
 		}
 		return () => (
 			<el-card>
 				<div class={$styles.title}>
-					<el-input v-model={title.value} size="large" placeholder="输入文章标题" />
+					<el-input v-model={searchForm.title} size="large" placeholder="输入文章标题" />
 					<el-button type="danger" size="large" onClick={openModel} style="margin-left: 10px">
 						发布文章
 					</el-button>
 				</div>
-				<MdEditor v-model={text.value} />
+				<MdEditor v-model={searchForm.content} />
+				<el-dialog v-model={visible.value}>
+					<el-form model={searchForm}>
+						<el-form-item label="文章分类">
+							<el-tag type="success" v-show={searchForm.categoryName} closable={true} onClose={removeCategory}>
+								{searchForm.categoryName}
+							</el-tag>
+							<el-popover placement="bottom-start" width="460" trigger="click" v-if="!article.categoryName">
+								<div class="popover-title">分类</div>
+								<div class="popover-container">{categorys.map((item) => item)}</div>
+								<el-button type="success" plain slot="reference" size="small">
+									添加分类
+								</el-button>
+							</el-popover>
+						</el-form-item>
+					</el-form>
+				</el-dialog>
 			</el-card>
 		)
 	},
